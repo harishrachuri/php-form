6c366977ed4288f1f0c79eb16dd3976b401df4bb
Harish Rachuri <harish@Media10>
2014-06-05 14:53:53 +0530

    added form files

---
+++ form1.php
@@ -0,0 +1,27 @@
+<html>
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<body>
+
+<form action="welcome.php" method="post">
+<div id="emp-form">
+<table class="form">
+<div class="header"><h3>Employee Registration Form</h3></div>
+<tr><td class="name">Employee No.:</td> <td class="value"><input type="text" name="empno"></td></tr>
+<tr><td class="name">First Name:   <td class="value"><input type="text" name="firstname"></td></tr>
+<tr><td class="name">Last Name:    <td class="value"><input type="text" name="lastname"></td></tr>
+<tr><td class="name">E-mail:       <td class="value"><input type="text" name="email"></td></tr>
+<tr><td class="name">Date of Birth: <td class="value"><input type="text" name="dob"></td></tr>
+<tr><td class="name">Phone Number:  <td class="value"><input type="text" name="phno"></td></tr>
+<tr><td class="name">Department:  <td class="value"><input type="text" name="dept"></td></tr>
+<tr><td class="name">Blood Group: <td class="value"><input type="text" name="bloodgrp"></td></tr>
+<tr><td class="name">State:       <td class="value"><input type="text" name="state"></td></tr>
+<tr><td class="name">City:        <td class="value"><input type="text" name="city"></td></tr>
+<tr><td class="name">Address:     <td class="value"><input type="text" name="addr"></td></tr>
+</table>
+<div class="submit"><input type="submit"></div>
+</div>
+</form>
+
+</body>
+</html>
+
